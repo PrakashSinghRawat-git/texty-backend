69b21c3f1ada191dbe7199687dc560ddad9754f3
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-04-14 21:22:47 +0530

    node index.js script

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const connectTOMongo = require('./db');
 const express = require('express');
 
 const cors = require('cors');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 // connecting to mongoDB
