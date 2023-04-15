35b04d188732fadbb797ba9066075dd97af450f1
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-04-15 11:07:26 +0530

    connecter to mongodb...

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/fallbackDB"
 
 const connectToMongo = ()=>{
-    mongoose.connect(MONGO_URI).then(()=>{console.log(`Connected to MongoDB (${MONGO_URI}) successfully...`)
+    mongoose.connect(MONGO_URI).then(()=>{console.log(`Connected to MongoDB successfully...`)
     }, (err)=>{
         console.log(MONGO_URI);
         
