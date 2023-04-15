1db5020b6a88341741d5f5adc6211c22aed551f0
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-04-15 11:03:12 +0530

    bcrypt js installation

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const fetchuser = require("../middleware/fetchuser");
 
